737227c9aea96468e1a3950f3b28b03634e1358e
hamada <hamada1207.daiki@gmail.com>
2021-03-06 16:29:07 +0900

    add logic to comHand for not lose

--- Janken/app/src/main/java/com/example/janken/ResultActivity.kt
+++ Janken/app/src/main/java/com/example/janken/ResultActivity.kt
@@ -2,6 +2,7 @@ package com.example.janken
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceManager
 import com.example.janken.databinding.ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
@@ -35,7 +36,7 @@ class ResultActivity : AppCompatActivity() {
         }
 
         // コンピュータの手
-        val comHand = (Math.random() * 3).toInt()
+        val comHand = getHand()
         when(comHand) {
             gu -> binding.comHanImage.setImageResource(R.drawable.com_gu)
             choki -> binding.comHanImage.setImageResource(R.drawable.com_choki)
@@ -50,5 +51,72 @@ class ResultActivity : AppCompatActivity() {
             2 -> binding.resultLabel.setText(R.string.result_lose)
         }
         binding.backButton.setOnClickListener{ finish() }
+
+        saveData(myHand, comHand, gameResult)
+    }
+
+    private fun saveData(myHand: Int, comHand: Int, gameResult: Int) {
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val gameCount = pref.getInt("GAME_COUNT", 0)
+        val winningStreakCount = pref.getInt("WINNING_STREAK_COUNT", 0)
+        val lastComHand = pref.getInt("LAST_COM_HAND", 0)
+        val lastGameResult = pref.getInt("LAST_GAME_RESULT", 0)
+
+        val editWinningStreakCount: Int = when {
+            lastGameResult == 2 && gameResult == 2 -> winningStreakCount + 1
+            else -> 0
+        }
+
+        val editor = pref.edit()
+        editor.putInt("GAME_COUNT", gameCount + 1)
+                .putInt("WINNING_STREAK_COUNT", editWinningStreakCount)
+                .putInt("LAST_MY_HAND", myHand)
+                .putInt("LAST_COM_HAND", comHand)
+                .putInt("BEFORE_LAST_COM_HAND", lastComHand)
+                .putInt("GAME_RESULT", gameResult)
+                .apply()
+    }
+
+    private fun getHand(): Int {
+        var hand = (Math.random() * 3).toInt()
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val gameCount = pref.getInt("GAME_COUNT", 0)
+        val winningStreakCount = pref.getInt("WINNING_STREAK_COUNT", 0)
+        val lastMyHand = pref.getInt("LAST_MY_HAND", 0)
+        val lastComHand = pref.getInt("LAST_COM_HAND", 0)
+        val beforeLastComHand = pref.getInt("BEFORE_LAST_COM_HAND", 0)
+        val gameResult = pref.getInt("GAME_RESULT", 0)
+
+        if (gameCount == 1) {
+            if (gameResult == 2) {
+                while (beforeLastComHand == hand) {
+                    hand = (Math.random() * 3).toInt()
+                }
+            } else if (gameResult == 1) {
+                hand = (lastMyHand - 1 + 3) % 3
+            }
+        } else if (winningStreakCount > 0) {
+            if (beforeLastComHand == lastComHand) {
+                while (lastComHand == hand) {
+                    hand = (Math.random() * 3).toInt()
+                }
+            }
+        }
+        return hand
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
