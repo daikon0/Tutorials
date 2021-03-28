96615c77f7bb6659439a73ec5d76202b3dd44071
hamada <hamada1207.daiki@gmail.com>
2021-03-28 19:09:59 +0900

    add MyJanken

---
+++ IosProjects/MyJanken/MyJanken/MyJankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyJankenApp.swift
+//  MyJanken
+//
+//  Created by 濵田　大輝 on 2021/03/28.
+//
+
+import SwiftUI
+
+@main
+struct MyJankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
