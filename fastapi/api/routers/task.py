b7c8d5686fc3b1ad4c3069e27fe0ac700a51a8c1
hamada <hamada1207.daiki@gmail.com>
2023-12-31 08:26:47 +0900

    Add routers

---
+++ fastapi/api/routers/task.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter
+
+router = APIRouter()
+
+@router.get("/tasks")
+async def get_tasks():
+  pass
+
+@router.post("/tasks")
+async def create_task():
+  pass
+
+@router.put("/tasks/{id}")
+async def update_task():
+  pass
+
+@router.delete("/tasks/{id}")
+async def delete_task():
+  pass
