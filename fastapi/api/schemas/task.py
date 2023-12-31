02c4e979f8045fd461ffb36181b62f2aba1f5123
hamada <hamada1207.daiki@gmail.com>
2023-12-31 13:19:08 +0900

    Add schemas

---
+++ fastapi/api/schemas/task.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, Field
+
+class TaskBase(BaseModel):
+    title: str | None = Field(None, example="クリーニングを取りに行く")
+
+class TaskCreate(TaskBase):
+    pass
+
+class TaskCreateResponse(TaskCreate):
+    id: int
+
+class Task(TaskBase):
+    id: int
+    done: bool = Field(False, description="完了フラグ")
+
+    class Config:
+      orm_mode = True
