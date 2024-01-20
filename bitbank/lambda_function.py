0ce104188bbe6f47407b0d261a1b602631eaef87
hamada <hamada1207.daiki@gmail.com>
2024-01-20 23:08:00 +0900

    Order ETH

--- bitbank/lambda_function.py
+++ bitbank/lambda_function.py
@@ -1,24 +1,30 @@
 import python_bitbankcc
 import os
 
-API_KEY = os.environ['BITBANK_API_KEY']
-API_SECRET = os.environ['BITBANK_API_SECRET']
+API_KEY = os.environ["BITBANK_API_KEY"]
+API_SECRET = os.environ["BITBANK_API_SECRET"]
 
-BUY_PRICE = 500
+BUY_PRICE = 1000
 
-def lambda_handler(event, context):
-    prv = python_bitbankcc.private(API_KEY, API_SECRET)
-    pub = python_bitbankcc.public()
+prv = python_bitbankcc.private(API_KEY, API_SECRET)
+pub = python_bitbankcc.public()
 
-    current_btc_price = float(pub.get_ticker('btc_jpy')['buy'])
-    target_btc_price = current_btc_price - 300
-    target_btc_amount = BUY_PRICE / target_btc_price
+
+def order(pair):
+    current_price = float(pub.get_ticker(pair)["buy"])
+    target_price = current_price - 100
+    target_amount = BUY_PRICE / target_price
 
     # 注文
     prv.order(
-        'btc_jpy', # ペア
-        target_btc_price, # 価格
-        target_btc_amount, # 注文枚数
-        'buy', # 注文サイド
-        'limit' # 注文タイプ
+        pair,  # ペア
+        target_price,  # 価格
+        target_amount,  # 注文枚数
+        "buy",  # 注文サイド
+        "limit",  # 注文タイプ
     )
+
+
+def lambda_handler(event, context):
+    order("btc_jpy")
+    order("eth_jpy")
